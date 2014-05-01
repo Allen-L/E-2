56bd4aed963ea480589a1842d9e7013446fc7bb2
Compx <mac@mactekiMacBook-Pro.local>
2014-05-01 16:42:05 +0800

    Initial Commit

---
+++ compxLostFind/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  compxLostFind
+//
+//  Created by Compx on 14-5-1.
+//  Copyright (c) 2014年 compx.com.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ compxLostFindTests/compxLostFindTests.m
@@ -0,0 +1,34 @@
+//
+//  compxLostFindTests.m
+//  compxLostFindTests
+//
+//  Created by Compx on 14-5-1.
+//  Copyright (c) 2014年 compx.com.cn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface compxLostFindTests : XCTestCase
+
+@end
+
+@implementation compxLostFindTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
